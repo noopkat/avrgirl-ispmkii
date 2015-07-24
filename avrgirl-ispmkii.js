8b0e20fd89ac2818f24f9d95431eb4ad8fbdb6ce
Suz Hinton <suz.hinton@gmail.com>
2015-07-23 22:58:18 -0400

    basic setup of module

--- avrgirl-ispmkii.js
+++ avrgirl-ispmkii.js
@@ -1,56 +1,131 @@
 var usb = require('usb');
-usb.setDebugLevel(0);
+var C = require('./lib/c');
 
-var VID = 0x03eb;
-var PID = 0x2104;
-var CMD_SIGN_ON = 0x01;
-var TOKEN = 0x0E;
+function avrgirlIspmkii() {
+  usb.setDebugLevel(0);
 
-/* There are several ways to parse MSB and LSB of the message 
- * I haven't decided on which way is best yet and I do not need to for the mkii
- */
-// var messageLen = new Buffer([0,1]);
-// messageLen.writeUInt16BE(0,0);
-//var messageLen = [((1 >> 8) & 0xFF), (1 & 0xFF)];
+  this.VID = 0x03eb;
+  this.PID = 0x2104;
 
-/* So it would be great if Atmel actually followed their own standard STK500v2 protocol, but no.
- * Considering that no one programs these manually except idiots like myself, AVR Studio would normally be just be taking
- * care of the headers/framing so the programmer can just follow it properly but whatever. 
- */
-var wholeMessage = [0x01];
-// this is what you would normally need
-// var wholeMessage = [0x1B, 0xff, messageLen[0], messageLen[1], TOKEN, CMD_SIGN_ON];
+  this.device = usb.findByIds(this.VID, this.PID);
 
-// no checksum needed either but here it is anyway
-// var checksum = 0;
-// for (var i = 0; i < wholeMessage.length; i += 1) {
-//   checksum ^= wholeMessage[i];
-// }
-// don't push as it's not needed
-//wholeMessage.push(checksum);
-var out = new Buffer(wholeMessage);
+  // *shakes fist at OSX*
+  (function(__open) {
+    device.__open = function() {
+      __open.call(this);
+      // injecting this line here to alleviate a bad error later
+      this.__claimInterface(0);
+    };
+  })(device.__open);
+};
 
-var device = usb.findByIds(VID, PID);
-// *shakes fist at OSX*
-(function(__open) { 
-  device.__open = function() {
-    __open.call(this);
-    this.__claimInterface(0);
-  };
-})(device.__open);
+avrgirlIspmkii.prototype.open = function() {
+  this.device.open();
+};
 
-device.open();
-var endpointOut = device.interfaces[0].endpoints[1];
-var endpointIn = device.interfaces[0].endpoints[0];
+avrgirlIspmkii.prototype.close = function() {
+  device.close();
+};
 
-console.log('sending:', wholeMessage);
+avrgirlIspmkii.prototype._setUpInterface = function() {
+  if (!this.device.interfaces) { return new Error('Failed to set up interfaces: device is not currently open.'); }
+  this.endpointOut = this.device.interfaces[0].endpoints[1];
+  this.endpointIn = this.device.interfaces[0].endpoints[0];
+};
 
-endpointOut.transfer(out, function(error) {
-  if (error) { return console.log('transfer error:', error); }
-  endpointIn.transfer(17, function(error, data) {
-    if (error) { return console.log('read error:', error); }
-    console.log('received data: ',  data.toString().replace('\n', ''));
-    device.close();
+avrgirlIspmkii.prototype._write = function (buffer, callback) {
+  if (!Buffer.isBuffer(buffer)) {
+    if (!Array.isArray(buffer)) {
+      return new Error('Failed to write: data was not Buffer or Array object.');
+    }
+    var buffer = new Buffer(buffer);
+  }
+  endpointOut.transfer(buffer, function (error) {
+    callback(error);
   });
-});
+};
 
+avrgirlIspmkii.prototype._read = function (length, callback) {
+  if (typeof length !== 'number') { return new Error('Failed to read: length must be a number.'); }
+  endpointIn.transfer(length, function (error, data) {
+    callback(error, data);
+  });
+};
+
+avrgirlIspmkii.prototype.getSignature = function (callback) {
+  var self = this;
+  var cmd = this.C.CMD_SIGN_ON;
+  var length = 17;
+
+  this._write(cmd, function (error) {
+    if (error) { callback(error); }
+    this._read(length, function(error, data) {
+      callback(error, data);
+    });
+  })
+};
+
+avrgirlIspmkii.prototype.verifySignature = function (data, callback) {
+  var error = null;
+  if (data[2] === self.C.STATUS_CMD_OK) {
+    var signature = data.slice(2);
+    if (signature.toString() !== 'AVRISP_MK2') {
+      error = new Error('Failed to verify: programmer signature does not match.');
+    }
+  } else {
+    error = new Error('Failed to verify: programmer return status was not OK.');
+  }
+  callback(error);
+};
+
+avrgirlIspmkii.prototype.writeFlash = function (options, data, callback) {
+  //options are [mode, delay, poll1, poll2]
+};
+
+avrgirlIspmkii.prototype.readFlash = function (length, cmd1, callback) {
+
+};
+
+avrgirlIspmkii.prototype.writeEeprom = function (options, data, callback) {
+  //options are [mode, delay, poll1, poll2]
+};
+
+avrgirlIspmkii.prototype.readEeprom = function (length, cmd1, callback) {
+
+};
+
+avrgirlIspmkii.prototype.readChipSignature = function (callback) {
+
+};
+
+avrgirlIspmkii.prototype.cmdSpiMulti = function (options, callback) {
+  //options are [cmd, numTx, numRx, rxStartAddr, txData]
+};
+
+avrgirlIspmkii.prototype.readFuse = function (length, cmd1, callback) {
+
+};
+
+avrgirlIspmkii.prototype.setParam = function (param, value, callback) {
+
+};
+
+avrgirlIspmkii.prototype.setParam = function (param, callback) {
+
+};
+
+avrgirlIspmkii.prototype.loadAddress = function (address, callback) {
+
+};
+
+avrgirlIspmkii.prototype.enterProgrammingMode = function (options, callback) {
+  // options are [timeout, stabDelay, cmdexeDelay, syncLoops, byteDelay, pollValue, pollIndex, cmd1, cmd2, cmd3, cmd4]
+};
+
+avrgirlIspmkii.prototype.exitProgrammingMode = function (predelay, postDelay, callback) {
+
+};
+
+avrgirlIspmkii.prototype.eraseChip = function (options, callback) {
+  //options are [eraseDelay, pollMethod, cmd1, cmd2, cmd3, cmd4]
+};
