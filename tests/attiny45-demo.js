a31ea3bb51ff5c3a91bd5b3e33e38f451b880853
Suz Hinton <suz.hinton@gmail.com>
2015-07-24 22:29:51 -0400

    attiny45 enter prog mode working

--- tests/attiny45-demo.js
+++ tests/attiny45-demo.js
@@ -1,6 +1,20 @@
 var AvrgirlIspmkii = require('../avrgirl-ispmkii');
 var avrgirl = new AvrgirlIspmkii();
 
+var attiny45 = {
+  timeout : 0xC8,
+  stabDelay : 0x64,
+  cmdexeDelay : 0x19,
+  syncLoops  : 0x20,
+  byteDelay : 0x00,
+  pollIndex : 0x03,
+  pollValue : 0x53,
+  predelay  : 0x01,
+  postdelay : 0x01,
+  pollmethod : 0x01,
+  pgmEnable: [0xAC, 0x53, 0x00, 0x00]
+};
+
 avrgirl.on('ready', function() {
   console.log('ready!');
   avrgirl.getSignature(function(error, data) {
@@ -8,7 +22,11 @@ avrgirl.on('ready', function() {
       avrgirl.verifySignature(data, function(error) {
         var status = (error) ? error : 'verified!';
         console.log(status);
-        avrgirl.close();
+        //avrgirl.close();
+        avrgirl.enterProgrammingMode(attiny45, function(error) {
+          var status = (error) ? error : 'progamming mode entered';
+          console.log(status);
+        });
       });
     }
   });
