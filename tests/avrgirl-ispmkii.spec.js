05a42fc791d8059535a2e66fda4d0af40b3e8528
Suz Hinton <suz.hinton@gmail.com>
2015-08-01 02:29:20 -0400

    better test assert labels

--- tests/avrgirl-ispmkii.spec.js
+++ tests/avrgirl-ispmkii.spec.js
@@ -1,7 +1,8 @@
 // harness
 var test = require('tape');
-var proxyquire =  require('proxyquire');
 // test helpers
+var sinon = require('sinon');
+var proxyquire = require('proxyquire');
 var chip = require('./helpers/mock-chip');
 var usbmock = require('./helpers/mock-usb');
 // module to test
@@ -9,20 +10,22 @@ var avrgirl = proxyquire('../avrgirl-ispmkii', { 'usb': usbmock });
 
 test('[ AVRGIRL-ISPMKII ] initialise', function (t) {
   var a = new avrgirl(chip);
-  t.equal(typeof a, 'object');
+  t.equal(typeof a, 'object', 'new is object');
   t.end();
 });
 
-test('[ AVRGIRL-ISPMKII ] emit ready', function (t) {
+test('[ AVRGIRL-ISPMKII ] device ready', function (t) {
   var a = new avrgirl(chip);
   a.on('ready', function() {
+    t.ok(typeof a.device === 'object', 'device created');
+    t.ok(a.device.interfaces.length, 'interfaces set up');
     t.pass('emitted "ready"');
     t.end();
   });
   t.timeoutAfter(500);
 });
 
-test('[ AVRGIRL-ISPMKII ] methods presence', function (t) {
+test('[ AVRGIRL-ISPMKII ] method presence', function (t) {
   var a = new avrgirl(chip);
   function isFn(name) {
     return typeof a[name] === 'function';
--- tests/c.spec.js
+++ tests/c.spec.js
@@ -4,6 +4,6 @@ var C = require('../lib/c');
 
 test('[ C ] length of props', function (t) {
   var cKeysLen = 46;
-  t.equal(Object.keys(C).length, cKeysLen);
+  t.equal(Object.keys(C).length, cKeysLen, 'length of props list is correct');
   t.end();
 });
