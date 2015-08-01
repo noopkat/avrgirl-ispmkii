f16c52545ae14d77a569ce54f239f07f6ed20079
Suz Hinton <suz.hinton@gmail.com>
2015-08-01 02:23:24 -0400

    make mock-usb interface state more realistic

--- tests/helpers/mock-usb.js
+++ tests/helpers/mock-usb.js
@@ -5,28 +5,32 @@ var usb = {};
 usb.setDebugLevel = function() {};
 
 usb.findByIds = function (vid, pid) {
-  return {
-    interfaces: [
-      {
-        endpoints: [
-          {
-            transfer: function (length, callback) {
-              var buffer = new Buffer(length);
-              return callback(null, buffer);
-            }
-          },
-          {
-            transfer: function (buffer, callback) {
-              return callback(null);
-            }
-          }
-        ]
-      }
-    ],
+  var device = {
+    interfaces: [],
      __open: function() {},
-    open: function() {},
-    close: function() {},
-  }
+  };
+
+  device.open = function() {
+    this.interfaces.push({
+      endpoints: [
+        {
+          transfer: function (length, callback) {
+            var buffer = new Buffer(length);
+            return callback(null, buffer);
+          }
+        },
+        {
+          transfer: function (buffer, callback) {
+            return callback(null);
+          }
+        }
+      ]
+    });
+  };
+  this.close = function() {
+    device.interfaces = [];
+  };
+  return device;
 };
 
 module.exports = usb;
